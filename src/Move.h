d06a9d79186ac9b4ef74baf1888c28bdf07a2b18
tunqtunq <lequangtung3112@gmail.com>
2026-06-06 21:11:36 +0700

    Add files via upload

---
+++ src/Move.h
@@ -0,0 +1,12 @@
+#ifndef MOVE_H
+#define MOVE_H
+
+// module Move quản lý thông tin về một nước đi trong trò chơi caro
+struct Move
+{
+    int row; // hàng của nước đi
+    int col; // cột của nước đi 
+    char symbol; // ký hiệu của người chơi (X hoặc O)
+};
+
+#endif
